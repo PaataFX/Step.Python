7ab9c38199c211fed69a7abf1eaa63861bf4988b
Paata_FX <paata.qurtiashvili@gmail.com>
2024-03-28 10:25:20 +0400

    29

---
+++ n29/Homework29.py
@@ -0,0 +1,110 @@
+import socket
+import threading
+import queue
+import sys
+import random
+import os
+
+# Client Code
+def ReceiveData(sock):
+    """
+    Function to continuously receive data from the server.
+
+    Args:
+        sock: Socket object used for communication.
+    """
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            print(data.decode('utf-8'))
+        except:
+            pass
+
+def RunClient(serverIP):
+    """
+    Function to run the client-side code.
+
+    Args:
+        serverIP: IP address of the server to connect to.
+    """
+    host = socket.gethostbyname(socket.gethostname())
+    port = random.randint(6000, 10000)
+    print('Client IP->' + str(host) + ' Port->' + str(port))
+    server = (str(serverIP), 5000)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    name = input('Please write your name here: ')
+    if name == '':
+        name = 'Guest' + str(random.randint(1000, 9999))
+        print('Your name is:' + name)
+    s.sendto(name.encode('utf-8'), server)
+    threading.Thread(target=ReceiveData, args=(s,)).start()
+    while True:
+        data = input()
+        if data == 'qqq':
+            break
+        elif data == '':
+            continue
+        data = '[' + name + ']' + '->' + data
+        s.sendto(data.encode('utf-8'), server)
+    s.sendto(data.encode('utf-8'), server)
+    s.close()
+    os._exit(1)
+
+# Client Code Ends Here
+
+
+# Server Code
+def RecvData(sock, recvPackets):
+    """
+    Function to continuously receive data on the server.
+
+    Args:
+        sock: Socket object used for communication.
+        recvPackets: Queue to store received packets.
+    """
+    while True:
+        data, addr = sock.recvfrom(1024)
+        recvPackets.put((data, addr))
+
+def RunServer():
+    """Function to run the server-side code."""
+    host = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    print('Server hosting on IP-> ' + str(host))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    clients = set()
+    recvPackets = queue.Queue()
+
+    print('Server Running...')
+
+    threading.Thread(target=RecvData, args=(s, recvPackets)).start()
+
+    while True:
+        while not recvPackets.empty():
+            data, addr = recvPackets.get()
+            if addr not in clients:
+                clients.add(addr)
+                continue
+            clients.add(addr)
+            data = data.decode('utf-8')
+            if data.endswith('qqq'):
+                clients.remove(addr)
+                continue
+            print(str(addr) + data)
+            for c in clients:
+                if c != addr:
+                    s.sendto(data.encode('utf-8'), c)
+    s.close()
+# Server Code Ends Here
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        RunServer()
+    elif len(sys.argv) == 2:
+        RunClient(sys.argv[1])
+    else:
+        print('Run Server:-> python Chat.py')
+        print('Run Client:-> python Chat.py <ServerIP>')
